aadf2228212beab02872665d0939cdc6a19d8003
lorinbeer <lorin@adobe.com>
2013-08-07 21:29:02 -0700

    expanded selection controller to handle new metadata syntax, and basic operation (add)

--- js/controllers.js
+++ js/controllers.js
@@ -1,31 +1,48 @@
-function SelectionCtrl($scope, $http) {
-    $scope.load = function() {
-         
-    }
+function selectionCtrl($scope, $http, ListerDataService) {
+    var entry = ListerDataService.pop();
+    var _data;
+    var selection = {};
+    selection.options = [];
+    $http.get('data/'+entry.uri+'.json').success(function(data) {
+        $scope.spec = data;
+        $scope.options = data.options;
+        $scope.cost = data.cost;
+        _data = data;
+    }); 
 
+    $scope.select = function(select) {
+        selection.options.push(select);
+        if (select.cost) {
+            $scope.cost = JSON.parse($scope.cost) + JSON.parse(select.cost);
+        }
+    }
 }
 
-
-function MainMenuCtrl($scope, $http) {
+function MainMenuCtrl($scope, $http, ListerDataService) {
     $http.get('data/mainmenu.json').success(function(data) {
-        console.log(data);
         $scope.MenuEntries = data;
-        console.log(data);
     });
-   
-    
  
     $scope.nav = function(entry) {
-        if (entry.method="nav" || !entry.method) {
+        $http.get('data/'+entry.uri+'.json').success(function(data) {
+                if (data.type=="nav" || !data.type) {
+                    data["ret"] = { "name" : "Back" };
+                    $scope.MenuEntries = data;
+                } else if (data.type=="select") {
+                    ListerDataService.push(entry);
+                    window.location.href = "#/selection/";
+                }
+            });
+
+        if (entry.action=="nav" || !entry.action) {
             $http.get('data/'+entry.uri+'.json').success(function(data) {
                 data["ret"] = { "name" : "Back" };
                 $scope.MenuEntries = data;
             });
-        } else if (entry.method="pull") {
-            $http.get('data/'+entry.uri+'.json').succuss(function(data) {                 
-                data["ret"] = { "name" : "Done", "uri" : "" };
-                $scope.MenuEntries = data;
-            });
+        } else if (entry.action=="pull") {
+            console.log(entry);
+            ListerDataService.push(entry);
+            window.location.href = "#/selection/";
         }
     } 
 }
