7c0b37cac48bc67a1babf50aa3a7c3425f769288
lorinbeer <lorin@adobe.com>
2013-08-07 21:29:38 -0700

    added ListerDataService to handle inter-controller communicatino through a simple queue

--- js/lister.js
+++ js/lister.js
@@ -1,7 +1,33 @@
-angular.module('lister', []).
-    config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-            when('/mainmenu', {templateUrl: 'partials/menu.html', controller: MainMenuCtrl}).
-            when('/selection', {templateUrl: 'partials/selection.html', controller: MainMenuCtrl}).
-            otherwise({redirectTo: '/mainmenu'});
-    }]);
+var Lister = 
+    angular.module('lister', []).
+        config(['$routeProvider', function($routeProvider) {
+            $routeProvider.
+                when('/mainmenu', {templateUrl: 'partials/menu.html', controller: MainMenuCtrl}).
+                when('/selection', {templateUrl: 'partials/selection.html', controller: selectionCtrl}).
+                otherwise({redirectTo: '/mainmenu'});
+        }]);
+
+Lister.factory('ListerDataService', function () {
+    var dataServiceObj = {
+        _dat : [],
+        _list: {
+            "total" : "0",
+            "entries" : []
+        },
+        push : function(obj) {
+           return dataServiceObj._dat.push(obj);
+        },
+        pop : function() {
+            return dataServiceObj._dat.pop();
+        },
+        clear : function() {
+            dataServiceObj._dat = [];
+        },
+        add : function(selection) {
+            dataServiceObj._list.total = dataServiceObj._list.total + selection.cost; 
+        }
+
+    };
+    return dataServiceObj;
+});
+
