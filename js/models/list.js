31fed515ba883ee6b51a8c45e70e7f65d9243be2
lorinbeer <lorin@adobe.com>
2013-08-07 22:17:57 -0700

    added js/models/ to contain eponymous implementations added list model

---
+++ js/models/list.js
@@ -0,0 +1,57 @@
+List = function () {
+    this._name = "";
+    this._options = [];
+}
+
+List.prototype.length = function () {
+    return this._options.length;
+}
+
+List.prototype.add = function (option) {
+    if (option.unique == 'true' || option.unique == undefined) {
+        if (this._options.indexOf(option) > 0) {
+            return;
+        }
+    }
+    this._options.push(option);
+}
+
+List.prototype.remove = function () {
+
+}
+
+List.prototype.find = function (option) {
+    
+}
+
+List.prototype.foreach = function (func) {
+    for (i in this._options) {
+        console.log(i);
+    }
+}
+
+List.prototype._indexOf = function (option) {
+    for (i in this._options) {
+        if (option === this._options[i]) {
+            return i;
+        }
+    }
+}
+
+
+function present(selection, option) {
+    for (var i = 0; i < selection.options.length; i = i+1) {
+        if (selection.options[i].name == option.name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function remove(selection, option) {
+    for (var i = 0; i < selection.options.length; i = i+1) {
+        if (selection.options[i].name == option.name) {
+            selection.options.splice(i, 1);
+        }
+    }
+}
