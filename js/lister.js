e1321c4764151fff79d9ff168e8713d39409e1ea
lorinbeer <lorin@adobe.com>
2013-08-11 02:58:58 -0700

    additional functions for navigation and history

--- js/lister.js
+++ js/lister.js
@@ -11,25 +11,43 @@ var Lister =
 Lister.factory('ListerDataService', function () {
     var dataServiceObj = {
         _dat : [],
-        _list: {
-            "total" : "0",
-            "entries" : []
+        _list: new List(),
+
+        getLast : function(action) {
+            for (var i = dataServiceObj._dat.length-1; i >= 0; i = i -1) {
+                if (dataServiceObj._dat[i].action == action) {
+                    return dataServiceObj._dat[i];
+                }
+            }  
         },
+
+        popToLast : function(action) {
+            for (var i = dataServiceObj._dat.length-1; i >= 0; i = i -1) {
+                if (dataServiceObj._dat[i].action == action) {
+                    return;
+                }
+                dataServiceObj._dat.pop();
+            }
+        },
+        
         push : function(obj) {
            return dataServiceObj._dat.push(obj);
         },
+
         pop : function() {
             return dataServiceObj._dat.pop();
         },
+
         peak : function () {
             return dataServiceObj._dat[dataServiceObj._dat.length-1];
         },
+
         clear : function() {
             dataServiceObj._dat = [];
         },
+
         add : function(selection) {
-            dataServiceObj._list.total = dataServiceObj._list.total + selection.cost; 
-            dataServiceObj._list.entries.push(selection); 
+            dataServiceObj._list.add(selection);
         }
 
     };
