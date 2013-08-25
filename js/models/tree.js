5481dbfbd2d3634c8d712dc40faa37857f81bdad
lorinbeer <lorin@adobe.com>
2013-08-25 14:07:30 -0700

    bread first search travelsal function _bft

--- js/models/tree.js
+++ js/models/tree.js
@@ -2,4 +2,20 @@ var Tree = function(root) {
     this._root = root ? root : new Node('root');
 }
 
+//adds 'node' as a child to tree-node with target id
+Tree.prototype.addChild = function(targetid, node) {
+    
+}
 
+// breadth first traversal
+Tree.prototype._bft = function(callback) {
+    var queue = [];
+    console.log(this);
+    // add root node's children
+    queue = queue.concat(this._root._children);
+    while (queue.length > 0) {
+        // always target first node in queue, add its children
+        queue = queue.concat(queue[0]._children);
+    }
+    
+}
