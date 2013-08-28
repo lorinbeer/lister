9ca70354dd774f60a4927266b9e156f53c796740
lorinbeer <lorin@adobe.com>
2013-08-28 16:24:32 -0700

    displays sublists correctly, respaces top level elements. Text correctly centered in label element

--- js/directives/listview.js
+++ js/directives/listview.js
@@ -4,14 +4,21 @@ Lister.directive("lsListView", function () {
         // restricts to use as an attribute
         restrict : 'A', 
         scope : '=',
-        template : '<div id="listview" class="entry" ng-repeat="child in tree._root._children">' +
+        replace : false,
+        template : '<div class="list" ng-repeat="child in tree._root._children">' +
+                     '<div class="spacer"></div>' +
+                     // the entry button
                      '<span class="label" ng-click="nav(child)">{{child._id}}</span>' +
+                    // the entry widget(s)
                      '<div id="{{child._id+\'_exp\'}}" ng-click="widget(\'expand\',child)" class="widget exp"></div>' +
+                    // subentry
+                        '<div id="{{child._id+\'_sublist\'}}" ng-repeat="descendant in child._children">' +
+                        '{{descendant._id}}' +
+                     '</div>' +
                    '</div>',
         link : function (scope, elem, attr) {
             console.log("lsListView directive 'link' function");
             console.log(scope, elem, attr);
-
 
             scope.$watch('expand', function (oldval, newval) {
 
