ca6cb4ea64e14315be4c872fd17adb4092576ec4
lorinbeer <lorin@adobe.com>
2013-08-29 06:40:59 -0700

    added license headers

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,21 @@
+/**
+ *  Lister UI Framework, create complex menu systems through a JSON metadata syntax
+ *  Copyright (C) 2013 Lorin Beer lorin.beer.dev@gmail.com
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function SelectionCtrl($scope, $http, $location, ListerDataService) {
     var entry = ListerDataService.peak(),
         _data,
