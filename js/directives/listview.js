5c220ac461876acb33d9b995f323afccc4740f26
lorinbeer <lorin@adobe.com>
2013-09-04 06:19:54 -0700

    moved listview directive template to separate file

--- js/directives/listview.js
+++ js/directives/listview.js
@@ -23,29 +23,12 @@ Lister.directive("lsListView", function () {
         restrict : 'A', 
         scope : '=',
         replace : false,
-        template : '<div class="list" ng-repeat="child in tree._root._children">' +
-                     '<div class="spacer"></div>' +
-                     // the entry button
-                     '<span class="label" ng-click="nav(child)">{{child._id}}</span>' +
-                    // the entry widget(s)
-                       '<div id="{{child._id+\'_exp\'}}" ng-click="widget(\'expand\',child)" class="widget exp"></div>' +
-                    // subentry
-                        '<div ng-switch="child.expand">' +
-                            '<div ng-switch-when="true">'+
-                                '<div id="{{child._id+\'_sublist\'}}" ng-repeat="subchild in child._children">'+
-                                    '<div>{{subchild._data.name}}</div>' +
-                                '</div>'+
-                            '</div>'+  
-                        '</div>' +
-                   '</div>',
+        templateURI: "listviewtemplate.html",
         link : function (scope, elem, attr) {
 
             scope.$watch('expand', function (oldval, newval) {
 
             });
         },
-        // specifies that we expect an object form the directive
-
-        //
     }
 });
