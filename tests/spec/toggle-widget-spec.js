50c9ff6a142549bb6b56089b05c2973336d8a8b1
lorinbeer <lorin@adobe.com>
2013-09-06 13:33:09 -0700

    toggle widget compiles node correctly with passed svg contents

--- tests/spec/toggle-widget-spec.js
+++ tests/spec/toggle-widget-spec.js
@@ -1,33 +1,36 @@
 describe("ToggleWidget", function() {
     var modulename = "lister",
-        scope,
+        officonurl = 'icons/svg/down_arrow.svg',
+        oniconurl = 'icons/svg/up_arrow.svg',
+        offsvg = '<circle cx="0" cy="0" r="50" fill="red"/>',
+        onsvg = '<rect width="50" height="50" style="fill:rgb(0,0,255)"/>',
+        $httpBackend,
+        $rootscope,
         html,
         element,
         compiled;
 
-    beforeEach(function () {
-        var mockmodule = angular.module('mocklister',['lister', 'ngMockE2E']);
-        mockmodule.run(function($httpBackend) {
-            $httpBackend.whenGET('icons/svg/down_arrow.svg').passThrough();
-        });
-        module('mocklister');
-        html = '<div ls-toggle-widget off="icons/svg/down_arrow.svg" on="up_arrow.svg"></div>';
-   
-        inject(function($compile, $rootScope) {
+    beforeEach(function() {
+        module(modulename);
+        inject(function($injector, $compile, $rootScope) {
+            $httpBackend = $injector.get('$httpBackend');
+            $httpBackend.whenGET(officonurl).respond(offsvg);
+            $httpBackend.whenGET(oniconurl).respond(onsvg);
+
+            html = '<div ls-toggle-widget off="icons/svg/down_arrow.svg" on="up_arrow.svg"></div>';
             scope = $rootScope.$new();
             element = angular.element(html);
-            $compile(element)(scope);
+            compiled = $compile(element)(scope);
             scope.$digest();
         });
+     });
+
+    it("should compile the html with the 'on' icon svg injected into the svg element", function () {
+        $httpBackend.flush();
+        console.log(compiled.children().children());
     });
 
-    it("", function () {
-        
+    it("should swap the svg contents on click", function () {
     });
 
-    it("", function () {
-    });
-
-    it("", function () {
-    });
 });
